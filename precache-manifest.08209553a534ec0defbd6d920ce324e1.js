aa49d48a2e319c598dd9cd3f8c2eadfc9d91a0ec
Kelly Araujo <kellymarquesdearaujo_@hotmail.com>
2020-10-19 21:27:59 -0300

    Updates

---
+++ precache-manifest.08209553a534ec0defbd6d920ce324e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "282382db898523588eecddb4ab647b26",
+    "url": "/deputados/index.html"
+  },
+  {
+    "revision": "6215130aca279b4316b1",
+    "url": "/deputados/static/css/main.0a385c8c.chunk.css"
+  },
+  {
+    "revision": "19feb3586691c488f573",
+    "url": "/deputados/static/js/2.8d7786d5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/deputados/static/js/2.8d7786d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6215130aca279b4316b1",
+    "url": "/deputados/static/js/main.361b42ad.chunk.js"
+  },
+  {
+    "revision": "934e15dd504b2735a61c",
+    "url": "/deputados/static/js/runtime-main.c64e1c6a.js"
+  }
+]);
